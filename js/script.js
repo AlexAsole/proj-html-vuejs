cfda252e238d80c932d85e2091d407c7894cb91a
Alex Giorgio Asole <alexgiorgio.asole@gmail.com>
2021-02-19 10:48:33 +0100

    primo blocco del main

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 new Vue({
     el: '#root',
     data: {
-        
+        menu: [
+            'HOME',
+            'SHOP',
+            'ABOUT',
+            'GALLERY',
+            'LOCATIONS',
+            'JOURNAL',
+            'CONTACT',
+            'MY ACCOUNT'
+        ] 
     },
 })
+Vue.config.devtools = true;
